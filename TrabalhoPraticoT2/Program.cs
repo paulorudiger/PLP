3a303f6e79a18a1ec64b6f4486198562f07d2e1a
paulorudiger <paulo.e.rudiger@gmail.com>
2024-03-26 23:44:12 -0300

    Iniciado T2 (ler arquivo e gerar relatorios)

---
+++ TrabalhoPraticoT2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using TrabalhoPraticoT2.Classes;
+
+var dataset = File.ReadAllText("..\\..\\..\\Dataset.csv");
+
+
+var produtosParse = ProdutoParser.ConverterLista(dataset);
+
+Console.WriteLine(produtosParse);
+
+for (int i = 0; i < produtosParse.Count; i++)
+{
+    Console.WriteLine(produtosParse[i].Descricao);
+
+}
