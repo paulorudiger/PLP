8d828d298a6b0df63d38be88b72a57b80682cdb8
paulorudiger <paulo.e.rudiger@gmail.com>
2024-04-09 21:58:45 -0300

    Iniciada API webdatabase

---
+++ APIWebDB/BaseDados/Models/ApidbContext.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIWebDB.BaseDados.Models;
+
+public partial class ApidbContext : DbContext
+{
+    public ApidbContext()
+    {
+    }
+
+    public ApidbContext(DbContextOptions<ApidbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TbCliente> TbClientes { get; set; }
+
+    public virtual DbSet<TbEndereco> TbEnderecos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=apidb;Username=postgres;Password=admin");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TbCliente>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("pk_tb_clientes");
+
+            entity.ToTable("tb_clientes");
+
+            entity.Property(e => e.Id)
+                .UseIdentityAlwaysColumn()
+                .HasColumnName("id");
+            entity.Property(e => e.Alteradoem)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("alteradoem");
+            entity.Property(e => e.Criadoem)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("criadoem");
+            entity.Property(e => e.Documento)
+                .IsRequired()
+                .HasMaxLength(20)
+                .HasColumnName("documento");
+            entity.Property(e => e.Nascimento)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("nascimento");
+            entity.Property(e => e.Nome)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnName("nome");
+            entity.Property(e => e.Telefone)
+                .HasMaxLength(20)
+                .HasColumnName("telefone");
+            entity.Property(e => e.Tipodoc)
+                .HasComment("0 - CPF\\n1 - CNPJ\\n2 - Passaporte\\n3 - CNH\\n99 - Outros")
+                .HasColumnName("tipodoc");
+        });
+
+        modelBuilder.Entity<TbEndereco>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("pk_tb_enderecos");
+
+            entity.ToTable("tb_enderecos");
+
+            entity.Property(e => e.Id)
+                .UseIdentityAlwaysColumn()
+                .HasColumnName("id");
+            entity.Property(e => e.Bairro)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnName("bairro");
+            entity.Property(e => e.Cep).HasColumnName("cep");
+            entity.Property(e => e.Cidade)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("cidade");
+            entity.Property(e => e.Clienteid).HasColumnName("clienteid");
+            entity.Property(e => e.Complemento)
+                .HasMaxLength(255)
+                .HasColumnName("complemento");
+            entity.Property(e => e.Logradouro)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("logradouro");
+            entity.Property(e => e.Numero)
+                .IsRequired()
+                .HasMaxLength(20)
+                .HasColumnName("numero");
+            entity.Property(e => e.Status)
+                .HasDefaultValue(1)
+                .HasComment("0 - inativo\\n1 - ativo")
+                .HasColumnName("status");
+            entity.Property(e => e.Uf)
+                .IsRequired()
+                .HasMaxLength(2)
+                .HasColumnName("uf");
+
+            entity.HasOne(d => d.Cliente).WithMany(p => p.TbEnderecos)
+                .HasForeignKey(d => d.Clienteid)
+                .HasConstraintName("fk_tb_enderecos_tb_clientes");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ APIWebDB/BaseDados/Models/TbCliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIWebDB.BaseDados.Models;
+
+public partial class TbCliente
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; }
+
+    public DateTime? Nascimento { get; set; }
+
+    public string Telefone { get; set; }
+
+    public string Documento { get; set; }
+
+    /// <summary>
+    /// 0 - CPF\n1 - CNPJ\n2 - Passaporte\n3 - CNH\n99 - Outros
+    /// </summary>
+    public int Tipodoc { get; set; }
+
+    public DateTime Criadoem { get; set; }
+
+    public DateTime Alteradoem { get; set; }
+
+    public virtual ICollection<TbEndereco> TbEnderecos { get; set; } = new List<TbEndereco>();
+}
