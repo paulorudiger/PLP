61f02db0b679bf25f90d5d97045a97bbd8536fd8
paulorudiger <paulo.e.rudiger@gmail.com>
2024-05-07 18:09:20 -0300

    Implementada tag <example> em enderecoDTO

--- APIWebDB/Services/DTOs/EnderecoDTO.cs
+++ APIWebDB/Services/DTOs/EnderecoDTO.cs
@@ -2,22 +2,41 @@
 {
     public class EnderecoDTO
     {
+        /// <example>
+        /// 89870000
+        /// </example>
         public int Cep { get; set; }
-
+        /// <example>
+        /// Rua Florianopolis
+        /// </example>
         public string Logradouro { get; set; }
-
+        /// <example>
+        /// 2830
+        /// </example>
         public string Numero { get; set; }
-
+        /// <example>
+        /// Apto
+        /// </example>
         public string Complemento { get; set; }
-
+        /// <example>
+        /// Santo Antonio
+        /// </example>
         public string Bairro { get; set; }
-
+        /// <example>
+        /// Pinhalzinho
+        /// </example>
         public string Cidade { get; set; }
-
+        /// <example>
+        /// SC
+        /// </example>
         public string Uf { get; set; }
-
+        /// <example>
+        /// 1
+        /// </example>
         public int Clienteid { get; set; }
-
+        /// <example>
+        /// 1
+        /// </example>
         /// <summary>
         /// 0 - inativo\n1 - ativo
         /// </summary>
